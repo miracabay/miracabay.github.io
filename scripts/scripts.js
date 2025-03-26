d002ea296a83e8052afcfa190bd442f7b06570f6
mirac <mirac@ASDQWE>
2025-03-26 22:42:15 +0300

    navbar update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Navbar ve Footer'ı yükle
     fetch("/layout/navbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
+
+            // Aktif sayfa linkine `current-page` sınıfını ekle
+            const currentPage = window.location.pathname.split("/").pop(); // Şu anki sayfa adını al
+            const navLinks = document.querySelectorAll(".navbar a"); // Navbar'daki tüm linkleri al
+
+            navLinks.forEach(link => {
+                if (link.getAttribute("href") === currentPage) {
+                    link.classList.add("current-page"); // Eğer şu anki sayfa linki ile eşleşiyorsa `current-page` ekle
+                }
+            });
         });
 
     fetch("/layout/footer.html")
