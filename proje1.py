77ce6367c44d44da27c573d4c7aa921737aa19b3
mirac <mirac@ASDQWE>
2025-03-26 21:51:40 +0300

    first commit

---
+++ proje1.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+numbers3 = [
+    Decimal("0.901101"), Decimal("0.0415"), Decimal("0.0578"), Decimal("1.187901"), Decimal("0.0406"),
+    Decimal("1.633402"), Decimal("1.537302"), Decimal("0.4283"), Decimal("0.3786"), Decimal("0.0578"),
+    Decimal("0.512301"), Decimal("0.3629"), Decimal("0.544401"), Decimal("0.1231"), Decimal("0.816601"),
+    Decimal("0.0476"), Decimal("1.244501"), Decimal("0.0297"), Decimal("0.0456"), Decimal("0.3038"),
+    Decimal("0.1243"), Decimal("0.1061"), Decimal("0.687701"), Decimal("0.1527"), Decimal("0.1707"),
+    Decimal("0.746601"), Decimal("0.0791"), Decimal("1.045301"), Decimal("0.0929"), Decimal("0.1299"),
+    Decimal("1.866802"), Decimal("0.1325"), Decimal("0.2159"), Decimal("1.633402"), Decimal("0.843001"),
+    Decimal("0.0333"), Decimal("0.0694"), Decimal("0.746601"), Decimal("0.3349"), Decimal("0.0133"),
+    Decimal("0.746601")
+]
+
+total3 = sum(numbers3)
+total3
